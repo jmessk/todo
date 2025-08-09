1c4133c12432973ed56df7410c63e7798b8215ce
jmessk <jmerusty@gmail.com>
2025-08-09 13:07:04 +0000

    Dockerfile.prod

--- server/src/domain/model/todo.py
+++ server/src/domain/model/todo.py
@@ -1,5 +1,10 @@
-class TodoItem:
-    def __init__(self, id: int, title: str, description: str | None = None):
-        self.id = id
-        self.title = title
-        self.description = description
+from pydantic import BaseModel
+from uuid import uuid4
+
+class TodoItem(BaseModel):
+    id: int
+    title: str
+    description: str | None = None
+    status: bool = False
+    created_at: str
+    updated_at: str | None = None
